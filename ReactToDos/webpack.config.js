add5c6debbcddb8c23719bc59fcca8a8c411443f
PandaFeeder <pandafeeder@gmail.com>
2016-09-07 00:23:53 +0800

    rebuild, nothing serious

--- ReactToDos/webpack.config.js
+++ ReactToDos/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
     },
-    devtool: 'source-map',
+    devtool: 'cheap-module-eval-source-map',
     plugins: [
     ],
     module: {
